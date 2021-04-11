88f87c79132102eb20daed18caf2841871cfbcf8
DaneeXIII <szarkadani0302@gmail.com>
2021-04-11 22:44:10 +0200

    hazifeladat

---
+++ app/src/main/java/com/example/hazifeladat/visszaAdas.java
@@ -0,0 +1,24 @@
+package com.example.hazifeladat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class visszaAdas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visszaadando);
+
+        TextView tv = findViewById(R.id.textviewMessage);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            tv.setText(extras.getString("message"));
+        }
+
+    }
+}
